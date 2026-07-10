0fc33263cd458622a607341acb2ad8398288590f
rkendel1 <148293244+rkendel1@users.noreply.github.com>
2026-07-10 19:05:14 -0400

    Adopt repository: src/pages/GamePage/components/GameOver.tsx

---
+++ src/pages/GamePage/components/GameOver.tsx
@@ -0,0 +1,26 @@
+import React, { FC } from 'react';
+import './GameOver.css';
+import { useGameStore } from '../../../model/store';
+import { Message } from './Message';
+import { TotalPacManDyingAnimationLength } from '../../../model/pacManDyingPhase';
+
+export const TOTAL_TIME_TO_GAME_OVER_MESSAGE = TotalPacManDyingAnimationLength;
+
+export const GameOver: FC = () => {
+  const pacManState = useGameStore((state) => state.game.pacMan.state);
+  const extraLivesLeft = useGameStore(
+    (state) => state.game.pacMan.extraLivesLeft
+  );
+  const diedAtTimestamp = useGameStore(
+    (state) => state.game.pacMan.diedAtTimestamp
+  );
+  const timestamp = useGameStore((state) => state.game.timestamp);
+
+  const isDead = pacManState === 'dead';
+  const isGameOver = isDead && extraLivesLeft === 0;
+  const timeSinceDeath = isDead ? timestamp - diedAtTimestamp : 0;
+  const gameOverMessageVisible =
+    isGameOver && timeSinceDeath >= TOTAL_TIME_TO_GAME_OVER_MESSAGE;
+
+  return gameOverMessageVisible ? <Message text="Game Over" /> : null;
+};
