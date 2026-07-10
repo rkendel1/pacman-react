ed22dc45825d9677a0a6350ef8613993335029b0
rkendel1 <148293244+rkendel1@users.noreply.github.com>
2026-07-10 19:05:16 -0400

    Adopt repository: src/pages/GamePage/components/GhostDebugControls.tsx

---
+++ src/pages/GamePage/components/GhostDebugControls.tsx
@@ -0,0 +1,43 @@
+import { Col, Row, Switch, Typography } from 'antd';
+import React, { FC } from 'react';
+import { useGameStore } from '../../../model/store';
+
+const { Text } = Typography;
+
+export const GhostDebugControls: FC = () => {
+  const showTarget = useGameStore(
+    (state) => state.debugState.ghostViewOptions.target
+  );
+  const showWayPoints = useGameStore(
+    (state) => state.debugState.ghostViewOptions.wayPoints
+  );
+  const setGhostViewOption = useGameStore((state) => state.setGhostViewOption);
+
+  return (
+    <div className="GhostDebugControls">
+      <Row>
+        <Col flex="0 0 56px">
+          <Switch
+            checked={showTarget}
+            onChange={(checked) => setGhostViewOption('target', checked)}
+          />
+        </Col>
+        <Col flex="0 0 auto">
+          <Text>Show Target Tile</Text>
+        </Col>
+        <Col flex="0 0 56px"></Col>
+
+        <Col flex="0 0 56px">
+          <Switch
+            checked={showWayPoints}
+            onChange={(checked) => setGhostViewOption('wayPoints', checked)}
+          />
+        </Col>
+        <Col flex="0 0 auto">
+          <Text>Show Waypoints</Text>
+        </Col>
+        <Col flex="0 0 56px"></Col>
+      </Row>
+    </div>
+  );
+};
