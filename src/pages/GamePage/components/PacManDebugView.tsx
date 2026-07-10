da8d7d2ec399a08c1bd238f3b17e0e28160bea66
rkendel1 <148293244+rkendel1@users.noreply.github.com>
2026-07-10 19:05:23 -0400

    Adopt repository: src/pages/GamePage/components/PacManDebugView.tsx

---
+++ src/pages/GamePage/components/PacManDebugView.tsx
@@ -0,0 +1,54 @@
+import { Button, Card, Space, Row, Col } from 'antd';
+import React, { FC } from 'react';
+import styled from 'styled-components';
+import { ghostCollidesWithPacMan } from '../../../model/detectCollisions';
+import { useGameStore } from '../../../model/store';
+
+export const PacManDebugView: FC = () => {
+  const pacManState = useGameStore((state) => state.game.pacMan.state);
+  const sendPacManEvent = useGameStore((state) => state.sendPacManEvent);
+
+  const isDead = pacManState === 'dead';
+  const isAlive = !isDead;
+
+  const handleKill = () => {
+    ghostCollidesWithPacMan(0); // Collide with first ghost
+  };
+
+  const handleRevive = () => {
+    sendPacManEvent('REVIVED');
+  };
+
+  return (
+    <Layout className="PacManDebugView">
+      <Card title="Pac Man" size="small" variant="borderless">
+        <Row>
+          <Col flex="0 0 104px">{`State: ${pacManState}`}</Col>
+
+          <Col flex="0 0 48px"></Col>
+
+          <Col flex="0 0 auto">
+            <Space>
+              {isAlive && (
+                <ButtonStyled shape="round" size="small" onClick={handleKill}>
+                  Kill
+                </ButtonStyled>
+              )}
+              {isDead && (
+                <ButtonStyled shape="round" size="small" onClick={handleRevive}>
+                  Revive
+                </ButtonStyled>
+              )}
+            </Space>
+          </Col>
+        </Row>
+      </Card>
+    </Layout>
+  );
+};
+
+const Layout = styled.div``;
+
+const ButtonStyled = styled(Button)`
+  min-width: 80px;
+`;
