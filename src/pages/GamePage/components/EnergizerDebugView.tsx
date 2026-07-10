8433580dfe8231817f384d70ec065fa2a829d56f
rkendel1 <148293244+rkendel1@users.noreply.github.com>
2026-07-10 19:05:08 -0400

    Adopt repository: src/pages/GamePage/components/EnergizerDebugView.tsx

---
+++ src/pages/GamePage/components/EnergizerDebugView.tsx
@@ -0,0 +1,49 @@
+import { Card, Button, Row, Col } from 'antd';
+import React, { FC } from 'react';
+import styled from 'styled-components';
+import { useGameStore } from '../../../model/store';
+import { eatEnergizer } from '../../../model/eatEnergizer';
+
+const formatter = new Intl.NumberFormat('en-US', {
+  minimumFractionDigits: 1,
+  maximumFractionDigits: 1,
+});
+
+export const EnergizerDebugView: FC = () => {
+  const energizerTimer = useGameStore((state) => state.game.energizerTimer);
+  const timeLeft = energizerTimer.duration - energizerTimer.timeSpent;
+
+  return (
+    <Layout className="EnergizerDebugView">
+      <Card title="Energizer" size="small" variant="borderless">
+        <Row>
+          <Col flex="0 0 200px">
+            <div>
+              Time left: {formatter.format(Math.abs(timeLeft) / 1000)} seconds
+            </div>
+          </Col>
+
+          <Col flex="0 0 48px"></Col>
+
+          <Col flex="0 0 80px">
+            <StyledButton
+              size="small"
+              shape="round"
+              onClick={() => {
+                eatEnergizer();
+              }}
+            >
+              Eat
+            </StyledButton>
+          </Col>
+        </Row>
+      </Card>
+    </Layout>
+  );
+};
+
+const Layout = styled.div``;
+
+const StyledButton = styled(Button)`
+  width: 80px;
+`;
