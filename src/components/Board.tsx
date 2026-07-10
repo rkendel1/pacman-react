7fa5f69bd219525f3ee2949c6d4fa3c7ef329556
rkendel1 <148293244+rkendel1@users.noreply.github.com>
2026-07-10 19:03:04 -0400

    Adopt repository: src/components/Board.tsx

---
+++ src/components/Board.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Board.css';
+import classNames from 'classnames';
+
+export const Board: React.FC<
+  React.PropsWithChildren<{ className?: string }>
+> = ({ className, children }) => (
+  <div className="BoardWrapper">
+    <div className={classNames('Board', className)}>{children}</div>
+  </div>
+);
