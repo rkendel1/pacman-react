20b1625fcf73f676a06a255250b7d16861a2dfa2
rkendel1 <148293244+rkendel1@users.noreply.github.com>
2026-07-10 19:05:12 -0400

    Adopt repository: src/pages/GamePage/components/GameDebugView.tsx

---
+++ src/pages/GamePage/components/GameDebugView.tsx
@@ -0,0 +1,56 @@
+import { Button, Card, Col, Row, Switch, Typography } from 'antd';
+import React, { FC } from 'react';
+import styled from 'styled-components';
+import { useGameStore } from '../../../model/store';
+
+const { Text } = Typography;
+
+export const GameDebugView: FC = () => {
+  const showHitBox = useGameStore(
+    (state) => state.debugState.gameViewOptions.hitBox
+  );
+  const gamePaused = useGameStore((state) => state.game.gamePaused);
+  const setGameViewOption = useGameStore((state) => state.setGameViewOption);
+  const setGamePaused = useGameStore((state) => state.setGamePaused);
+  const resetGame = useGameStore((state) => state.resetGame);
+
+  return (
+    <Layout className="PacManDebugView">
+      <Card title="Game" size="small" variant="borderless">
+        <Row>
+          <Col flex="0 0 56px">
+            <Switch
+              checked={showHitBox}
+              onChange={(checked) => setGameViewOption('hitBox', checked)}
+            />
+          </Col>
+          <Col flex="0 0 auto">
+            <Text>Show Hit Boxes</Text>
+          </Col>
+          <Col flex="0 0 48px"></Col>
+
+          <Col flex="0 0 56px">
+            <Switch
+              checked={gamePaused}
+              onChange={(checked) => setGamePaused(checked)}
+            />
+          </Col>
+          <Col flex="0 0 auto">
+            <Text>Paused</Text>
+          </Col>
+          <Col flex="0 0 48px"></Col>
+
+          <ButtonStyled size="small" onClick={() => resetGame()} shape="round">
+            Restart
+          </ButtonStyled>
+        </Row>
+      </Card>
+    </Layout>
+  );
+};
+
+const Layout = styled.div``;
+
+const ButtonStyled = styled(Button)`
+  min-width: 80px;
+`;
