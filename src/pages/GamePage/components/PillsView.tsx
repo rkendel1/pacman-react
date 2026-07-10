5426bf898b751b253eef3a0971a9aad84be04088
rkendel1 <148293244+rkendel1@users.noreply.github.com>
2026-07-10 19:05:26 -0400

    Adopt repository: src/pages/GamePage/components/PillsView.tsx

---
+++ src/pages/GamePage/components/PillsView.tsx
@@ -0,0 +1,80 @@
+import React, { FC, memo } from 'react';
+import { Box } from '../../../components/Box';
+import { Sprite } from '../../../components/Sprite';
+import {
+  addCoordinatesAndVector,
+  ScreenCoordinates,
+  screenFromTile,
+  SCREEN_TILE_CENTER_VECTOR,
+  TileCoordinates,
+} from '../../../model/Coordinates';
+import { getPillHitBox } from '../../../model/detectCollisions';
+import {
+  BASIC_PILL_ID,
+  ENERGIZER_ID,
+  MAZE_HEIGHT_IN_TILES,
+  MAZE_WIDTH_IN_TILES,
+  EMPTY_TILE_ID,
+} from '../../../model/MazeData';
+import { useGameStore } from '../../../model/store';
+
+const BasicPillView: FC<{ position: ScreenCoordinates }> = ({ position }) => (
+  <Sprite x={position.x - 10} y={position.y - 10} name="basic-pill" />
+);
+
+const EnergizerView: FC<{ position: ScreenCoordinates }> = ({ position }) => (
+  <Sprite x={position.x - 10} y={position.y - 10} name="energizer" />
+);
+
+export const BasicPillHitBox: FC = () => {
+  const rect = getPillHitBox({ x: 1, y: 3 });
+  return <Box rect={rect} color="blue" />;
+};
+
+const PillView: FC<{ tile: TileCoordinates }> = ({ tile }) => {
+  const { x, y } = tile;
+  const tileId = useGameStore((state) => state.game.maze.pills[y][x]);
+
+  if (tileId === BASIC_PILL_ID) {
+    return (
+      <BasicPillView
+        position={addCoordinatesAndVector(
+          screenFromTile(tile),
+          SCREEN_TILE_CENTER_VECTOR
+        )}
+      />
+    );
+  }
+  if (tileId === ENERGIZER_ID) {
+    return (
+      <EnergizerView
+        position={addCoordinatesAndVector(
+          screenFromTile(tile),
+          SCREEN_TILE_CENTER_VECTOR
+        )}
+      />
+    );
+  }
+  return null;
+};
+
+// Performance tricks used here:
+// Make each PillView subscribe to only its specific tile.
+// Make PillsView a React.memo to prevent any rerenders.
+// Also: Create PillView only for those coordinates where there is a pill on first render.
+export const PillsView: FC = memo(() => {
+  const pills = useGameStore((state) => state.game.maze.pills);
+
+  return (
+    <>
+      {Array.from({ length: MAZE_HEIGHT_IN_TILES }).map((_, y) =>
+        Array.from({ length: MAZE_WIDTH_IN_TILES }).map((_, x) => {
+          const pillFound = pills[y][x] !== EMPTY_TILE_ID;
+          return pillFound && <PillView key={`${x}/${y}`} tile={{ x, y }} />;
+        })
+      )}
+    </>
+  );
+});
+
+PillsView.displayName = 'displayName';
