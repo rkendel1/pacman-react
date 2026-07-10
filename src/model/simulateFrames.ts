bdd0aeebc06c84e69101a4e79b4e5a05ec03429b
rkendel1 <148293244+rkendel1@users.noreply.github.com>
2026-07-10 19:04:42 -0400

    Adopt repository: src/model/simulateFrames.ts

---
+++ src/model/simulateFrames.ts
@@ -0,0 +1,38 @@
+import { useGameStore } from './store';
+import { onAnimationFrame, TYPICAL_FRAME_LENGTH } from './onAnimationFrame';
+import { MilliSeconds } from './Types';
+import { SCREEN_TILE_SIZE } from './Coordinates';
+
+const framesPerTile = () => {
+  const speed = useGameStore.getState().game.speed;
+  return SCREEN_TILE_SIZE / speed;
+};
+
+export const simulateFrame = (
+  frameLength: MilliSeconds = TYPICAL_FRAME_LENGTH
+) => {
+  const previousTimestamp = useGameStore.getState().game.externalTimeStamp ?? 0;
+  const timestamp = previousTimestamp + frameLength;
+  onAnimationFrame(timestamp);
+};
+
+export const simulateFrames = (numberOfFrames: number) => {
+  for (let frames = 0; frames < numberOfFrames; frames++) {
+    simulateFrame();
+  }
+};
+
+export const simulateFramesToMoveNTiles = (numberOfTiles: number) => {
+  const numberOfFrames = numberOfTiles * framesPerTile();
+  simulateFrames(numberOfFrames);
+};
+
+export const simulateTime = (timeToPass: MilliSeconds) => {
+  const numberOfFrames = Math.floor(timeToPass / TYPICAL_FRAME_LENGTH);
+  simulateFrames(numberOfFrames);
+  const passedTime = numberOfFrames * TYPICAL_FRAME_LENGTH;
+  const timeLeft = timeToPass - passedTime;
+  if (timeLeft > 0) {
+    simulateFrame(timeLeft);
+  }
+};
