6d092eb75cf34855ecdbfa343b0f3ab2236bbf16
rkendel1 <148293244+rkendel1@users.noreply.github.com>
2026-07-10 19:05:06 -0400

    Adopt repository: src/pages/GamePage/GamePage.tsx

---
+++ src/pages/GamePage/GamePage.tsx
@@ -0,0 +1,94 @@
+import { Row } from 'antd';
+import React, { useEffect } from 'react';
+import styled from 'styled-components';
+import { Board } from '../../components/Board';
+import { DebugView } from './components/DebugView';
+import { ExtraLives } from './components/ExtraLives';
+import { GameOver } from './components/GameOver';
+import { GhostsGameView } from './components/GhostsView';
+import { MazeView } from './components/MazeView';
+import { PacManView } from './components/PacManView';
+import { PillsView } from './components/PillsView';
+import { Score } from './components/Score';
+import { useGameStore } from '../../model/store';
+import { useKeyboardActions } from './components/useKeyboardActions';
+import { VSpace } from '../../components/Spacer';
+import { useGameLoop } from '../../model/useGameLoop';
+
+export const GamePage: React.FC = () => {
+  const resetGame = useGameStore((state) => state.resetGame);
+  const setGamePaused = useGameStore((state) => state.setGamePaused);
+
+  useEffect(() => {
+    resetGame();
+    return () => {
+      setGamePaused(true);
+    };
+  }, []);
+
+  useGameLoop();
+  useKeyboardActions();
+
+  return (
+    <Layout data-testid="GamePage">
+      <ScoreArea>
+        <Row justify="center">
+          <Score />
+        </Row>
+        <VSpace size="small" />
+      </ScoreArea>
+
+      <EmptyArea />
+
+      <BoardArea>
+        <Board>
+          <MazeView />
+          <PillsView />
+          <PacManView />
+          <GhostsGameView />
+          <GameOver />
+        </Board>
+        <VSpace size="large" />
+        <Row justify="center">
+          <ExtraLives />
+        </Row>
+      </BoardArea>
+
+      <DebugArea>
+        <DebugView />
+      </DebugArea>
+    </Layout>
+  );
+};
+
+const Layout = styled.div`
+  margin-left: 16px;
+  margin-right: 16px;
+
+  display: grid;
+
+  @media (max-width: 592px) {
+    padding-top: max(16px, env(safe-area-inset-top));
+  }
+
+  @media (min-width: 1280px) {
+    grid-template-columns: 1fr 1fr;
+    justify-items: center;
+  }
+  @media (max-width: 1280px) {
+    grid-template-columns: 1fr;
+    justify-items: center;
+  }
+`;
+
+const ScoreArea = styled.div``;
+
+const EmptyArea = styled.div``;
+
+const BoardArea = styled.div``;
+
+const DebugArea = styled.div`
+  @media (max-width: 1280px) {
+    display: none;
+  }
+`;
