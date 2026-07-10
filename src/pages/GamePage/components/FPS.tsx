89a9e03c8d4a3e1a090e0693f58c70fdbb7c80ee
rkendel1 <148293244+rkendel1@users.noreply.github.com>
2026-07-10 19:05:10 -0400

    Adopt repository: src/pages/GamePage/components/FPS.tsx

---
+++ src/pages/GamePage/components/FPS.tsx
@@ -0,0 +1,16 @@
+import React, { FC } from 'react';
+import { useGameStore } from '../../../model/store';
+import styled from 'styled-components';
+
+export const FPS: FC<{ className?: string }> = ({ className }) => {
+  const lastFrameLength = useGameStore((state) => state.game.lastFrameLength);
+  return (
+    <Layout className={className}>
+      {Math.round(1000 / lastFrameLength)} FPS
+    </Layout>
+  );
+};
+
+const Layout = styled.div`
+  margin-top: 12px;
+`;
