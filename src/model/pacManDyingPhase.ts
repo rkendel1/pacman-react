dbaa96612f8841a6480dbe130fb12defc43f332e
rkendel1 <148293244+rkendel1@users.noreply.github.com>
2026-07-10 19:04:41 -0400

    Adopt repository: src/model/pacManDyingPhase.ts

---
+++ src/model/pacManDyingPhase.ts
@@ -0,0 +1,20 @@
+import { MilliSeconds } from './Types';
+
+export type PacManDyingPhase = number;
+export const PacManDyingPhaseCount = 13;
+export const PacManDyingPhases: PacManDyingPhase[] = Array.from(
+  Array(PacManDyingPhaseCount).keys()
+);
+export const PacManDyingPhaseLength: MilliSeconds = 200;
+export const TotalPacManDyingAnimationLength: MilliSeconds =
+  PacManDyingPhaseLength * PacManDyingPhaseCount;
+
+export const getPacManDyingPhase = (
+  timeSinceDeath: MilliSeconds
+): PacManDyingPhase => {
+  let dyingPhase: number = Math.floor(timeSinceDeath / PacManDyingPhaseLength);
+  if (dyingPhase >= PacManDyingPhaseCount) {
+    dyingPhase = PacManDyingPhaseCount - 1;
+  }
+  return dyingPhase as PacManDyingPhase;
+};
